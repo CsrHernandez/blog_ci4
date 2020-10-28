aa92b0aa781cfff7846fb6ecdb0828c777364fbe
DevCesarHdez <dev.cesar.hdez@gmail.com>
2020-10-28 13:07:51 -0600

    Model Post, Prueba de insert posts

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1,14 +1,17 @@
 <?php namespace App\Controllers;
 
 use App\Models\UsersModel;
+use App\Models\PostsModel;
 
 class Dashboard extends BaseController
 {
 	private $usersModel = null;
+	private $postsModel = null;
 
 	public function __construct()
 	{
 		$this->usersModel = new UsersModel();
+		$this->postsModel = new PostsModel();
 	}
 
 	public function index()
@@ -25,6 +28,23 @@ class Dashboard extends BaseController
 		echo "</pre>";*/
 
 		$this->loadViews('index');
+	}
+
+	public function insertPost()
+	{
+		$id = $this->postsModel->insert([
+			'banner'	 => 'img.php',
+			'title'		 => 'New Post',
+			'intro'	 	 => 'Hello this is me',
+			'content'	 => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			'category'   => 1,
+			'tags'		 => 'sports',
+			'created_by' => 1
+		]);
+
+		echo "<pre>";
+		print_r($id);
+		echo "</pre>";
 	}
 
 	//--------------------------------------------------------------------
---
+++ app/Models/PostsModel.php
@@ -0,0 +1,27 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+/**
+ *
+ */
+class PostsModel extends Model
+{
+    protected $table      = 'posts';
+    protected $primaryKey = 'id';
+
+    protected $retunType      = 'array';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = ['banner', 'title', 'intro', 'content', 'category', 'tags', 'created_by'];
+
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
+
+    function __construct()
+    {
+        // code...
+    }
+
+}
